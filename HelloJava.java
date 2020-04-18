22b9e397653e0048b3806efbf672540feb578e3e
Ubuntu <janayo@gmail.com>
2020-04-18 15:30:17 +0000

    modifying HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,7 @@
 class HelloJava {
 public static void main(String args[]){
 System.out.Println(“ I am your Java Program. Thank you! ”);
+System.out.Println(“ this line was added in the develop branch ”);
 }
 }
 
